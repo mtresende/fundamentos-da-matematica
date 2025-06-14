d6c1bdb41581b89df68aeeac80a0a48dc2606568
Lucasgfoli <lucasgabrielfer.oli@gmail.com>
2025-06-14 14:27:23 -0300

    Entrada de dados no procedimento - Atividade 1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,44 @@
 ﻿using System;
 
-class Program {
+class Program
+{
 
     //Distância entre pontos:
 
-    static void DistanciaEntrePontos(int xPonto1, int yPonto1, int xPonto2, int yPonto2)
+    static void DistanciaEntrePontos()
     {
+        //Declaração das váriaveis
+        int xPonto1, yPonto1, xPonto2, yPonto2;
+
+        Console.WriteLine("Digite a coordenada X do ponto 1:");
+            xPonto1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a coordenada Y do ponto 1:");
+            yPonto1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a coordenada X do ponto 2:");
+            xPonto2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a coordenada Y do ponto 2:");
+            yPonto2 = int.Parse(Console.ReadLine());
+
         //Fórmula para calcular a distância entre dois pontos: d = √((xB - xA)² + (yB - yA)²)
         //Onde A se trata do Ponto 1 e B o Ponto 2
-        
+
         double distancia = Math.Sqrt(Math.Pow((xPonto2 - xPonto1), 2) + Math.Pow((yPonto2 - yPonto1), 2));
 
         Console.WriteLine("A distância entre o ponto 1 ({0}, {1}) e o ponto 2({2}, {3}) é: {4:F2}", xPonto1, yPonto1, xPonto2, yPonto2, distancia);
     }
 
 
-    public static void EqReta () {
+    public static void EqReta()
+    {
         // Fórmula: y = ax + b
 
-        Console.WriteLine ("Caucule a equação de reta a partir de dois pontos");
-        Console.Write ("Digite o ponto 1 (x1,y1): ");
+        Console.WriteLine("Calcule a equação de reta a partir de dois pontos");
+        Console.Write("Digite o ponto 1 (x1,y1): ");
         string[] ponto1 = Console.ReadLine().Split(',');
         double x1 = double.Parse(ponto1[0]);
         double y1 = double.Parse(ponto1[1]);
 
-        Console.Write ("Digite o ponto 2 (x2, y2): ");
+        Console.Write("Digite o ponto 2 (x2, y2): ");
         string[] ponto2 = Console.ReadLine().Split(',');
         double x2 = double.Parse(ponto2[0]);
         double y2 = double.Parse(ponto2[1]);
@@ -44,25 +58,20 @@ class Program {
         */
         double b = y1 - a * x1;
 
-        if (b >= 0) {
-            Console.WriteLine ($"A equação da reta é: y = {a}x + {b}");
-        } else {
-            Console.WriteLine ($"A equação da reta é: y = {a}x {b}");
+        if (b >= 0)
+        {
+            Console.WriteLine($"A equação da reta é: y = {a}x + {b}");
+        }
+        else
+        {
+            Console.WriteLine($"A equação da reta é: y = {a}x {b}");
         }
     }
 
-    public static void Main (string[] args) {
+    public static void Main(string[] args)
+    {
 
-        Console.WriteLine("Digite a coordenada X do ponto 1:");
-            int xPonto1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Digite a coordenada Y do ponto 1:");
-            int yPonto1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Digite a coordenada X do ponto 2:");
-            int xPonto2 = int.Parse(Console.ReadLine());        
-        Console.WriteLine("Digite a coordenada Y do ponto 2:");
-            int yPonto2 = int.Parse(Console.ReadLine());
-
-        DistanciaEntrePontos(xPonto1, yPonto1, xPonto2, yPonto2);
+        DistanciaEntrePontos();
 
         EqReta();
     }
