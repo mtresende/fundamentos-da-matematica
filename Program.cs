3e4ca7890d5599b87f6deaeefcc6404a6d97685b
Matheus Resende <matheus.resende2003@gmail.com>
2025-06-14 09:59:42 -0300

    adicionando equação de reta

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program {
+    public static void EqReta () {
+        Console.WriteLine ("Caucule a equação de reta a partir de dois pontos");
+        Console.Write ("Digite o ponto 1 (x1, y1): ");
+        string[] ponto1 = Console.ReadLine().Split(',');
+        double x1 = double.Parse(ponto1[0]);
+        double y1 = double.Parse(ponto1[1]);
+
+        Console.Write ("Digite o ponto 2 (x2, y2): ");
+        string[] ponto2 = Console.ReadLine().Split(',');
+        double x2 = double.Parse(ponto2[0]);
+        double y2 = double.Parse(ponto2[1]);
+
+        // Fórmula: y = ax + b
+
+        double a = (y2 - y1) / (x2 - x1);
+        double b = y1 - a * x1;
+
+        Console.WriteLine ($"A equação da reta é: y = {a}x + {b}");
+    }
+
+    public static void Main (string[] args) {
+        EqReta();
+    }
+}
